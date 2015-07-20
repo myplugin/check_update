88c8a1d6c65d77c6c6bf89b4537c4a58f3991d30
myplugin <shopthuytruc@yahoo.com>
2015-07-20 15:20:19 +0700

    Create new_version.php

---
+++ new_version.php
@@ -0,0 +1,10 @@
+<?php
+/******************************************************************
+Plugin Name: Creating our Scheduled Event
+Description: Creating our Scheduled Event
+Version: 1
+Author: b
+*/
+
+Nội dung code mới
+?>
